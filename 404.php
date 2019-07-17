c0e8036d2590e54607e271a913d6fdb6ad538000
PSekishov <pasha-sveta@yandex.ru>
2019-07-17 18:51:27 +0300

    add files

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="error">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<img src="<?php echo get_template_directory_uri().'/images/error_404.png' ?>">
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
