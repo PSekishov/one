8803b496bf58e8abfcd9c9506c19310fa07898a7
PSekishov <pasha-sveta@yandex.ru>
2019-07-17 14:45:47 +0300

    add walker end function for theme one menu

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /****** include scripts ******/
 
 add_action('wp_enqueue_scripts','theme_one_include_scripts');
@@ -14,4 +15,32 @@ wp_enqueue_script('myscript',get_template_directory_uri().'/js/myscript.js',null
 
 }
 
-/*****  ******/
+
+/***** register menu ******/
+
+add_action('after_setup_theme','register_menu_theme_one');
+
+function register_menu_theme_one(){
+	register_nav_menus([
+    'top_menu'=>'Главное меню',
+    'footer_menu'=>'Меню в подвале',
+	]);
+}
+
+
+/***** function main menu *****/
+ 
+
+add_action('main_menu',function(){
+
+  require_once('/inc/bs4navwalker.php');
+  
+	wp_nav_menu([
+	'theme_location'  => 'top_menu',
+	'container_class' => 'collapse navbar-collapse', 
+	'container_id'    => 'navbarSupportedContent',
+	'menu_class'      => 'navbar-nav',
+	'walker'          => new bs4navwalker(),
+]);
+
+});
--- header.php
+++ header.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?> >
-  
+<html lang="<?php language_attributes(); ?>">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Document</title>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?> >
 
-  
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
 
+					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+						
+						<a class="navbar-brand" href="<?php home_url(); ?>"><?php bloginfo('name'); ?></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+						</button>
 
+					    <?php do_action('main_menu'); ?>
+
+					</nav>
+				
+				</div>
+			</div>
+		</div>
